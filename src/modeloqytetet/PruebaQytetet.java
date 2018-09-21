fc0ae3707aebc195a996bdabc831b0f784b49ad9
manu <manu@ManuMINT>
2018-09-21 13:09:04 +0200

    Añadidos métodos que filtran las cartas y probados en el main.

--- src/modeloqytetet/PruebaQytetet.java
+++ src/modeloqytetet/PruebaQytetet.java
@@ -15,11 +15,67 @@ import java.util.Iterator;
 public class PruebaQytetet {
     static Qytetet juego = new Qytetet();
     
-    public static void main(String []args){
+    public PruebaQytetet(){
         juego.inicializarCartasSorpresa();
+    }
+    
+    private ArrayList ValorMayorQueCero(){
+        ArrayList<Sorpresa> mazo = juego.getMazo();
+        ArrayList<Sorpresa> mayorquecero = new ArrayList<>();
         
-        for(int i = 0; i < juego.getSize(); ++i){
-            System.out.println(juego.getMazo(i).toString());
+        for(int i = 0; i < mazo.size(); ++i){
+            if(mazo.get(i).getValor() > 0)
+                mayorquecero.add(mazo.get(i));
         }
+        
+        return mayorquecero;
+    }
+    
+    private ArrayList CartasIrA(){
+        ArrayList<Sorpresa> mazo = juego.getMazo();
+        ArrayList<Sorpresa> iracasilla = new ArrayList<>();
+        
+        for(int i = 0; i < mazo.size(); ++i){
+            if(mazo.get(i).getTipo() == TipoSorpresa.IRACASILLA)
+                iracasilla.add(mazo.get(i));
+        }
+        
+        return iracasilla;
+    }
+    
+    private ArrayList FiltrarTipo(TipoSorpresa tipo){
+        ArrayList<Sorpresa> mazo = juego.getMazo();
+        ArrayList<Sorpresa> iracasilla = new ArrayList<>();
+        
+        for(int i = 0; i < mazo.size(); ++i){
+            if(mazo.get(i).getTipo() == tipo)
+                iracasilla.add(mazo.get(i));
+        }
+        
+        return iracasilla;
+    }
+    
+    
+    public static void main(String []args){
+        PruebaQytetet prueba = new PruebaQytetet();
+        
+        ArrayList<Sorpresa> mazo = juego.getMazo();
+        
+        for(Sorpresa s: mazo)
+            System.out.println(s);
+        
+        ArrayList<Sorpresa> mayoresquecero = prueba.ValorMayorQueCero();
+        
+        System.out.println("\nCartas con valor mayor que 0:");
+        
+        for(Sorpresa s: mayoresquecero)
+            System.out.println(s);
+        
+        ArrayList<Sorpresa> cartasira = prueba.CartasIrA();
+        
+        System.out.println("\nCartas del tipo IrA:");
+        
+        for(Sorpresa s: cartasira)
+            System.out.println(s);
     }
 }
