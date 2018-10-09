f718c59283d73273ddccb2333edfe1e7524f7dc0
manu <manu@ManuMINT>
2018-10-09 12:51:35 +0200

    Declarados todos los métodos de "TituloPropiedad" aunque no implementados

--- src/modeloqytetet/TituloPropiedad.java
+++ src/modeloqytetet/TituloPropiedad.java
@@ -13,6 +13,7 @@ public class TituloPropiedad {
     private int precioEdificar;
     private int numHoteles;
     private int numCasas;
+    private Jugador propietario;
     
     public TituloPropiedad(String name, int precio, int alquiler, float factorRevalorizacion,  int hipotecaBase, int precioEdificar){
     this.nombre = name;
@@ -24,6 +25,7 @@ public class TituloPropiedad {
     hipotecada = false;
     numHoteles = 0;
     numCasas = 0;
+    propietario = null;
     }
     
     public String getnombre(){ return nombre; }
@@ -43,6 +45,102 @@ public class TituloPropiedad {
     public int getnumHoteles() {return numHoteles; }
     
     public int getnumCasas() {return numCasas; }
+    
+    Jugador getPropietario(){
+        return propietario;
+    }
+    
+    void setPropietario(Jugador propietario){
+        this.propietario = propietario;
+    }
+    
+    int calcularCosteCancelar(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int calcularCosteHipotecar(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int calcularImporteAlquiler(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int calcularPrecioVenta(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    void cancelarHipoteca(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    void cobrarAlquiler(int coste){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    void edificarCasa(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    void edificarHotel(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int getAlquilerBase(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    float getFactorRevalorizacion(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int getHipotecaBase(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    boolean getHipotecada(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    String getNombre(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int getNumCasas(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int getNumHoteles(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int getPrecioCompra(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int getPrecioEdificar(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int hipotecar(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    int pagarAlquiler(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    boolean propietarioEncarcelado(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    boolean setHipotecada(boolean hipotecada){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
+    
+    boolean tengoPropietario(){
+        throw new UnsupportedOperationException("Sin implementar");
+    }
 
     @Override
     public String toString() {
